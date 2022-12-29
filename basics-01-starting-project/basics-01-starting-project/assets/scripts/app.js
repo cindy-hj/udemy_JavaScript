76e6813ec22c04c61f055ea347be7baf47b2a80a
cindy-hj <hj0304hj@gmail.com>
2022-12-29 22:04:36 +0900

    2022 12 29 22:03
    till 34

--- basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) { // 함수가 전역변수를 이용해서 값을 반환 하는것은 bad, 전역변수 값을 수정하는건 ok
+    currentResult = currentResult + userInput.value;
+}
+
+addBtn.addEventListener('click', add); // 클릭 했을때 add 함수를 찾아서 호출해라!
